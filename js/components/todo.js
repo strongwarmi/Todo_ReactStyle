76b394157ab3c91bc770766b9349f64a047a9246
strongwarmi <elianasc10@hotmail.com>
2017-06-05 16:21:52 -0500

    muestra lista

---
+++ js/components/todo.js
@@ -0,0 +1,59 @@
+'use strict';
+const TodoItem= (data,update) => {
+    const todo = $('<diiv class="todo">');
+    const checkbox = $ ('<input type = "checkbox">');
+    const span = $('<span>' + data.text + '</span>');
+    const remove = $ ('<button>Remove</button>');
+    todo.append(checkbox);
+    todo.append(span);
+    todo.append(remove);
+    checkbox.on('change',(e) => {
+        data.completed = !data.completed;
+        update();
+    });
+    remove.on('click',(e) => {
+       const idx = state.todos.map(x => x.text).indexOf(data.text);
+        state.todos.splice(idx,1);
+        update();
+    });
+    return todo;
+}
+const reRender = (todoList,completedList)=>{
+    todoList.empty();
+    completedList.empty();
+    state.todos.forEach(todo => {
+       if(!todo.completed) {
+           todoList.append(TodoItem(todo,_ =>{reRender(todoList,completedList);}));
+       }else{
+           completedList.append(TodoItem(todo,_ =>{reRender(todoList,completedList);}));
+       }
+    });
+}
+const Todo = () => {
+    const parent = $('<div class="white-card"></div>');
+    const input = $ ('<input id="input-item" type="text" placeholder="Ingresa la tarea">');
+    const todoTitle = $('<p>To Do Items:</p>');
+    const list = $('<div class="list"></div>');
+    const hr = $('<hr>');
+    const completedTitle = $('<p>Completed Items</p>');
+    const completedList = $('<div class="completed"></div>');
+    parent.append(input);
+    parent.append(todoTitle);
+    parent.append(list);
+    parent.append(hr);
+    parent.append(completedTitle);
+    parent.append(completedList);
+    input.on('keypress',(e)=>{
+        if(e.which === 13){
+            if(input.val() != ""){
+                state.todos.push({
+                    text:input.val(),
+                    completed:false                    
+                });
+                input.val('');
+                reRender(list,completedList);
+            }
+        }
+    });
+    return parent;
+}
